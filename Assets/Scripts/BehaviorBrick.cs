44153c590adefe0eebc1a67320dd3d49ecf27726
haringma15 <martin.haring2@edu.fh-joanneum.at>
2019-01-03 18:57:29 +0100

    Add rigidbody to bricks

---
+++ Assets/Scripts/BehaviorBrick.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviorBrick : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update() {
+        checkInput();
+    }
+
+    void checkInput() {
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) move(-1);
+        if (Input.GetKeyDown(KeyCode.RightArrow)) move(1);
+        if (Input.GetKeyDown(KeyCode.UpArrow)) rotate(90);
+    }
+
+    void move(float x) {
+        this.gameObject.transform.position += new Vector3(x, 0 , 0);
+    }
+
+    void rotate(float z) {
+        this.gameObject.transform.Rotate(0, 0, z);
+    }
+}
