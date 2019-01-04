fd169684d315b037a97fd3fe3e218b01251e3d87
haringma15 <martin.haring2@edu.fh-joanneum.at>
2019-01-04 21:28:24 +0100

    Implement correct brick movement and rotation in a grid

--- Assets/Scripts/BehaviorBrick.cs
+++ Assets/Scripts/BehaviorBrick.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class BehaviorBrick : MonoBehaviour
 {
+    public bool isRotationAllowed = true;
+    public bool isRotationLimited = false;
+    public int moveVal = 1;
+    public int rotateVal = 90;
     public float fallSpeed = 1;
     private float fallTime = 0;
 
@@ -13,14 +17,14 @@ public class BehaviorBrick : MonoBehaviour
     }
 
     void checkInput() {
-        if (Input.GetKeyDown(KeyCode.LeftArrow)) move(-1);
-        if (Input.GetKeyDown(KeyCode.RightArrow)) move(1);
-        if (Input.GetKeyDown(KeyCode.UpArrow)) rotate(90);
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && isValidMove(-moveVal, 0, 0)) move(-moveVal);
+        if (Input.GetKeyDown(KeyCode.RightArrow) && isValidMove(moveVal, 0, 0)) move(moveVal);
+        if (isRotationAllowed && Input.GetKeyDown(KeyCode.UpArrow) && isValidMove(0, 0, rotateVal)) rotate(rotateVal);
     }
 
     void fallDown() {
-        if (Time.time - fallTime >= fallSpeed) {
-            this.gameObject.transform.position += new Vector3(0, -1, 0);
+        if (Time.time - fallTime >= fallSpeed && isValidMove(0, -moveVal, 0)) {
+            this.gameObject.transform.position += new Vector3(0, -moveVal, 0);
             fallTime = Time.time;
         }
     }
@@ -30,6 +34,19 @@ public class BehaviorBrick : MonoBehaviour
     }
 
     void rotate(float z) {
-        this.gameObject.transform.Rotate(0, 0, z);
+        if (isRotationLimited && this.gameObject.transform.rotation.eulerAngles.z >= rotateVal) this.gameObject.transform.Rotate(0, 0, -z);
+        else this.gameObject.transform.Rotate(0, 0, z);
+    }
+
+    bool isValidMove(float moveX, float moveY, float rotateZ) {
+        bool isValid = true;
+        Quaternion currentRotation = this.gameObject.transform.rotation;                                    // save current rotation
+        rotate(rotateZ);                                                                                    // rotate the brick into its next form
+        foreach (Transform cube in this.gameObject.transform) {
+            Vector3 attemptedPos = cube.position + new Vector3(moveX, moveY, 0);                            // move each cube of the brick into their next position
+            if (FindObjectOfType<BehaviorGrid>().isInsideGrid(attemptedPos) == false) isValid = false;      // if they are out of bounds, mark the move invalid
+        }
+        this.gameObject.transform.rotation = currentRotation;                                               // reset the rotation
+        return isValid;
     }
 }
