2b34770b490db1a13fa5baf9e7c6f9f6799b144b
haringma15 <martin.haring2@edu.fh-joanneum.at>
2019-01-06 20:35:57 +0100

    Implement brick instantiation

---
+++ Assets/Scripts/BrickInstantiater.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickInstantiater : MonoBehaviour
+{
+    public GameObject[] bricks;
+
+    void Start() {
+        instantiateNextBrick();
+    }
+
+    void Update() {
+        
+    }
+
+    void instantiateNextBrick() {
+        GameObject nextBrick = (GameObject)Instantiate(bricks[Random.Range(0, bricks.Length)], new Vector3(5, 20, 0), Quaternion.identity);
+    }
+}
