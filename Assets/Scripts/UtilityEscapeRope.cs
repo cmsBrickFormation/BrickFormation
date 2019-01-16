850a4b7b90cb4d7218510e4ac28479f10211296e
haringma15 <martin.haring2@edu.fh-joanneum.at>
2019-01-16 01:43:53 +0100

    Implement instant quit functionality, create basic how to play scene

---
+++ Assets/Scripts/UtilityEscapeRope.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UtilityEscapeRope : MonoBehaviour
+{
+    public string emergencyExit = "MainMenu";
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) FindObjectOfType<UtilitySceneManager>().loadScene(emergencyExit);
+    }
+}
