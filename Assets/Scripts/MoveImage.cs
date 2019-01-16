032e5be1455bcb3c62d103dea2c3e4acf11d9c78
SuperCari <carina.coelestin@edu.fh-joanneum.at>
2019-01-17 00:43:08 +0100

    Add Design & UX & Music & co

---
+++ Assets/Scripts/MoveImage.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveImage : MonoBehaviour
+{
+    public GameObject logo;
+    private int spinSpeed;
+    private float timer = 0;
+
+    void Update()
+    {
+        moveLogo(logo);
+        timer++;
+
+    }
+
+    void moveLogo(GameObject logo)
+    {
+         if (timer < 140) logo.GetComponent<RectTransform>().anchoredPosition = new Vector2(logo.GetComponent<RectTransform>().anchoredPosition.x, logo.GetComponent<RectTransform>().anchoredPosition.y - 4);
+
+    } 
+}
+
